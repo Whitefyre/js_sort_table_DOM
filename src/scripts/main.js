1ec7a7127de0cd1710722aedbf64b38aba153bd6
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-09-02 15:08:57 +0300

    delete DESC

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,100 +3,43 @@
 const tableBody = document.querySelector('tbody');
 const headRow = document.querySelector('thead').querySelector('tr');
 const people = [...tableBody.children];
-let ascendingAge = true;
-let ascendingSalary = true;
-let ascendingName = true;
-let ascendingPosition = true;
 
-function positionSort() {
-  let sortedPosition = [];
+function compareNum(i) {
+  const result = [...people].sort(
+    (a, b) =>
+      Number(a.children[i].textContent.replace(/[$,]/g, '')) -
+      Number(b.children[i].textContent.replace(/[$,]/g, '')),
+  );
 
-  if (ascendingPosition) {
-    sortedPosition = [...people].sort((a, b) =>
-      a.children[1].textContent.localeCompare(b.children[1].textContent),);
-    ascendingPosition = false;
-  } else {
-    sortedPosition = [...people].sort((a, b) =>
-      b.children[1].textContent.localeCompare(a.children[1].textContent),);
-    ascendingPosition = true;
-  }
-
-  for (const person of sortedPosition) {
-    tableBody.append(person);
-  }
+  return result;
 }
 
-function nameSort() {
-  let sortedName = [];
+function compareString(i) {
+  const result = [...people].sort((a, b) =>
+    a.children[i].textContent.localeCompare(b.children[i].textContent),
+  );
 
-  if (ascendingName) {
-    sortedName = [...people].sort((a, b) =>
-      a.children[0].textContent.localeCompare(b.children[0].textContent),);
-    ascendingName = false;
-  } else {
-    sortedName = [...people].sort((a, b) =>
-      b.children[0].textContent.localeCompare(a.children[0].textContent),);
-    ascendingName = true;
-  }
-
-  for (const person of sortedName) {
-    tableBody.append(person);
-  }
+  return result;
 }
 
-function salarySort() {
-  let sortetSalary = [];
+function sort(columnIndex, compareFunction) {
+  let sorted = [];
 
-  if (ascendingSalary) {
-    sortetSalary = [...people].sort(
-      (a, b) =>
-        Number(a.children[3].textContent.replace(/[$,]/g, '')) -
-        Number(b.children[3].textContent.replace(/[$,]/g, '')),
-    );
-    ascendingSalary = false;
-  } else {
-    sortetSalary = [...people].sort(
-      (a, b) =>
-        Number(b.children[3].textContent.replace(/[$,]/g, '')) -
-        Number(a.children[3].textContent.replace(/[$,]/g, '')),
-    );
-    ascendingSalary = true;
-  }
+  sorted = compareFunction(columnIndex);
 
-  for (const person of sortetSalary) {
-    tableBody.append(person);
-  }
-}
-
-function ageSort() {
-  let sortedAge = [];
-
-  if (ascendingAge) {
-    sortedAge = [...people].sort(
-      (a, b) => a.children[2].textContent - b.children[2].textContent,
-    );
-
-    ascendingAge = false;
-  } else {
-    sortedAge = [...people].sort(
-      (a, b) => b.children[2].textContent - a.children[2].textContent,
-    );
-    ascendingAge = true;
-  }
-
-  for (const person of sortedAge) {
+  for (const person of sorted) {
     tableBody.append(person);
   }
 }
 
 headRow.addEventListener('click', (e) => {
   if (e.target.textContent === 'Age') {
-    ageSort();
+    sort(2, compareNum);
   } else if (e.target.textContent === 'Salary') {
-    salarySort();
+    sort(3, compareNum);
   } else if (e.target.textContent === 'Name') {
-    nameSort();
+    sort(0, compareString);
   } else {
-    positionSort();
+    sort(1, compareString);
   }
 });

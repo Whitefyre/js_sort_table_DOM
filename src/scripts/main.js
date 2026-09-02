1144d4b182b1f7448c3eefe7c44aa41c0b5e657d
Bohdan Kulbaka <bkulbaka1990@gmail.com>
2026-09-02 15:27:24 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,28 +4,19 @@ const tableBody = document.querySelector('tbody');
 const headRow = document.querySelector('thead').querySelector('tr');
 const people = [...tableBody.children];
 
-function compareNum(i) {
-  const result = [...people].sort(
-    (a, b) =>
-      Number(a.children[i].textContent.replace(/[$,]/g, '')) -
-      Number(b.children[i].textContent.replace(/[$,]/g, '')),
+function compareNum(a, b, i) {
+  return (
+    Number(a.children[i].textContent.replace(/[$,]/g, '')) -
+    Number(b.children[i].textContent.replace(/[$,]/g, ''))
   );
-
-  return result;
 }
 
-function compareString(i) {
-  const result = [...people].sort((a, b) =>
-    a.children[i].textContent.localeCompare(b.children[i].textContent),
-  );
-
-  return result;
+function compareString(a, b, i) {
+  return a.children[i].textContent.localeCompare(b.children[i].textContent);
 }
 
 function sort(columnIndex, compareFunction) {
-  let sorted = [];
-
-  sorted = compareFunction(columnIndex);
+  const sorted = [...people].sort((a, b) => compareFunction(a, b, columnIndex));
 
   for (const person of sorted) {
     tableBody.append(person);
